2905b03cce6d34834d820a19579789bdd8599b60
[Evan Paul Gilmore] <sculptoid11@gmail.com>
2023-03-27 18:35:27 -0400

    Continued work on user input, added an asynchronous function and learned about them.

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
-const fs = require('fs'); 
+const fs = require('fs');
 const inquirer = require('inquirer');
 
-const readMeFile = 
+async function generateReadMe() {
+  const readMeFile =
     inquirer
-        .prompt([
-            {
-            type: 'input',
-            title: 'Title',
-            message: 'What is the project title?'
-            },
-            {
-            type: 'input',
-            title: 'Description',
-            message: 'Please enter a project description'
-            },
-            {
-            type: 'input',
-            title: 'Installation',
-            message: 'Please enter the project installation instructions.'
-            },
-            {
-            type: 'input',
-            title: 'Usage',
-            message: 'Please enter instructions for the users of the project.'
-            },
-            {
-            type: 'input',
-            title: 'Contributing',
-            message: 'Please enter any contributing guidelines for the project.'
-            },
-            {
-            type: 'input',
-            title: 'Tests',
-            message: 'Please enter any testing instructions for the project.'
-            },
-        ])
-const callback = function(err){
-  err ? console.error(err) : console.log('Commit logged!')
+      .prompt([
+        {
+          type: 'input',
+          title: 'title',
+          message: 'What is the project title?'
+        },
+        {
+          type: 'input',
+          title: 'description',
+          message: 'Please enter a project description'
+        },
+        {
+          type: 'input',
+          title: 'installation',
+          message: 'Please enter the project installation instructions.'
+        },
+        {
+          type: 'input',
+          title: 'usage',
+          message: 'Please enter instructions for the users of the project.'
+        },
+        {
+          type: 'input',
+          title: 'contributing',
+          message: 'Please enter any contributing guidelines for the project.'
+        },
+        {
+          type: 'input',
+          title: 'tests',
+          message: 'Please enter any testing instructions for the project.'
+        },
+      ]);
+  const readMeContents = `
+  # ${readMeFile.title}
+
+  ## Description 
+
+  ${readMeFile.descripion}
+
+  ## Installation 
+
+  ${readMeFile.installation}
+
+  ## Usage
+
+  ${readMeFile.usage}
+
+  ##Tests
+
+  ${readMeFile.tests}
+  `; 
+
+  const callback = function (err) {
+    err ? console.error(err) : console.log('Commit logged!')
+  }
+  fs.writeFile("readme.md", readMeContents, callback)
 }
-fs.writeFile("readme.md", readMeFile, callback)
+
+generateReadMe();
